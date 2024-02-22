a3f86635443bdf222b3e5ce13586504610f05ed2
fadil_abdi <mohfadilabdillah1302@gmail.com>
2024-02-22 22:13:17 +0700

    nmbh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,6 +29,9 @@
         <div>
             <h1>read data mahasiswa</h1>
         </div>
+        <div>
+            <h1>ini percobaan</h1>
+        </div>
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
